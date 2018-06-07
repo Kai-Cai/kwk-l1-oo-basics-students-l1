adc6215ddcbb12997f1e6a95a370ca3e38cf5efe
Kai-Cai <kaiaschiff@gmail.com>
2018-06-07 22:16:58 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
-# book.rb
-# Add you Book class here
+class Book
+  
+  def initialize(title)
+    @title
+    end
+
+  def title
+    @title
+  end
+
+
+end
+
+
+book_1=Book.new("And Then There Were None")
+
+puts book_1.title
