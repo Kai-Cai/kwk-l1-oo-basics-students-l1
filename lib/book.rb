cba5b98c30a6c1eb13da0d2d4213a915d74767d0
Kai-Cai <kaiaschiff@gmail.com>
2018-06-07 22:20:29 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,6 +2,7 @@ class Book
   
   def initialize(title)
     @title = title
+    @author = author
     end
 
   def title
@@ -12,6 +13,6 @@ class Book
 end
 
 
-book_1=Book.new("And Then There Were None")
+book_1=Book.new("And Then There Were None", "Agatha Cristie")
 
 puts book_1.title
